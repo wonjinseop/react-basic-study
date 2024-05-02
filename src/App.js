68c3f1865304f3a5fc706c43fe18edee12a56ee0
wonjinseop <wonjinseop7@naver.com>
2024-05-02 16:56:47 +0900

    9. 동적 리스트 렌더링, 연도별 필터링

--- src/App.js
+++ src/App.js
@@ -7,19 +7,28 @@ function App() {
   //지출 항목 객체 배열
   const expenses = [
     {
+      id: 1,
       title: '바나나',
       price: 2000,
-      date: new Date(2023, 3 - 1, 23),
+      date: new Date(2021, 3 - 1, 23),
     },
     {
+      id: 2,
       title: 'BBQ치킨',
       price: 20000,
-      date: new Date(2023, 5 - 1, 21),
+      date: new Date(2022, 5 - 1, 21),
     },
     {
+      id: 3,
       title: '도미노피자',
       price: 35000,
       date: new Date(2023, 7 - 1, 4),
+    },
+    {
+      id: 4,
+      title: '엽기떡볶이',
+      price: 18000,
+      date: new Date(2024, 5 - 1, 1),
     },
   ];
 
@@ -27,6 +36,7 @@ function App() {
   const addExpenseHandler = (newExpense) => {
     console.log('App 컴포넌트에서 응답함!');
     console.log('newExpense: ', newExpense);
+    expenses.push(newExpense);
   };
 
   return (
--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
 
-const Expenses = ({ items: expenses }) => {
+const Expenses = ({ items }) => {
+  // ExpenseItem을 동적으로 렌더링하기.
+  // const iterateExpenseItem = () => {
+  //   // 콜백 함수의 매개값으로 배열의 요소가 하나씩 전달됨.
+  //   // 콜백 함수는 배열 요소만큼 반복됨
+  //   // map의 리턴값: 함수가 적용된 각 요소가 담긴 새로운 배열이 리턴됨.
+  //   return items.map((item) => (
+  //     <ExpenseItem title={item.title} price={item.price} date={item.date} />
+  //   ));
+  // };
+
+  const [filteredYear, setFilteredYear] = useState(
+    new Date().getFullYear().toString(),
+  );
+
+  // 자식 컴포넌트 ExpenseFilter에 있는 선택 연도를 끌어올리는 함수.
+  const changedFilterHandler = (selectedYear) => {
+    // console.log('ExpenseFilter에서 응답함!');
+    // console.log('year: ', selectedYear);
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <div className="expenses">
-      <ExpenseItem
-        title={expenses[0].title}
-        price={expenses[0].price}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        price={expenses[1].price}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        price={expenses[2].price}
-        date={expenses[2].date}
-      />
-    </div>
+    <Card clasName="expenses">
+      <ExpenseFilter onChangeFilter={changedFilterHandler} />
+      {items
+        .filter((item) => item.date.getFullYear().toString() === filteredYear)
+        .map((item) => (
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            price={item.price}
+            date={item.date}
+          />
+        ))}
+    </Card>
   );
 };
 
