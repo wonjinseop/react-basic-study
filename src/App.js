26e283a77c3d44eced1bb3516c38edf88adf33c7
wonjinseop <wonjinseop7@naver.com>
2024-05-02 11:56:19 +0900

    4. 컴포넌트 분할하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import React from 'react'
-import ExpenseItem from './components/ExpenseItem';
+import React from 'react';
+import Expenses from './components/Expenses';
 
 function App() {
 
@@ -10,25 +10,23 @@ function App() {
     {
       title: '바나나',
       price: 2000,
-      date: new Date(2023, 3, 23),
+      date: new Date(2023, 3 - 1, 23),
     },
     {
       title: 'BBQ치킨',
       price: 20000,
-      date: new Date(2023, 5, 21),
+      date: new Date(2023, 5 - 1, 21),
     },
     {
       title: '도미노피자',
       price: 35000,
-      date: new Date(2023, 7, 4),
+      date: new Date(2023, 7 - 1, 4),
     },
   ];
 
   return (
     <>
-      <ExpenseItem title={expenses[0].title} price={expenses[0].price} date={expenses[0].date} />
-      <ExpenseItem title={expenses[1].title} price={expenses[1].price} date={expenses[1].date} />
-      <ExpenseItem title={expenses[2].title} price={expenses[2].price} date={expenses[2].date} />
+      <Expenses items={expenses}/>
     </>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,8 @@
 import React from 'react';
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = ({title, price, date}) => {
-
-  // 한자리 수를 두자리로 변환해주는 함수
-  const make2digit = (text) => {
-    return text.toString().padStart(2, '0');
-  }
-
-  // 날짜 포맷팅 변환 함수
-  const makeFormattedDate = () => {
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDate();
-
-    return `${year}년 ${make2digit(month)}월 ${make2digit(day)}일`;
-  }
 
   // 숫자를 화폐 표기법으로 바꾸기
   const formattedPrice = new Intl.NumberFormat('ko-KR').format(price);
@@ -23,7 +10,7 @@ const ExpenseItem = ({title, price, date}) => {
 
   return (
     <div className='expense-item'>
-      <div>{makeFormattedDate()}</div>
+      <ExpenseDate date={date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
         <div className='expense-item__price'>{formattedPrice}원</div>
