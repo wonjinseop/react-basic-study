cf2fe299c6aafc94ae54e5dc5d29d9760f840400
wonjinseop <wonjinseop7@naver.com>
2024-05-03 17:15:27 +0900

    Reac portal 개념 및 활용

--- src/components/UI/Modal/ErrorModal.js
+++ src/components/UI/Modal/ErrorModal.js
@@ -3,23 +3,41 @@ import styles from './ErrorModal.module.css';
 import Card from '../Card';
 import Button from '../Button/Button';
 
+import ReactDOM from 'react-dom';
+
+const BackDrop = ({ onConfirm }) => {
+  return <div className={styles.backdrop} onClick={onConfirm} />;
+};
+
+const ModalOverlay = ({ title, message, onConfirm }) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 // Modal을 사용하는 쪽에서 모달 제목과 메세지가 props로 전달될 것이다.
 // onConfirm -> AddUsers쪽에서 상태관리하고 있는 모달 노출 여부를 제어하는 함수.
 const ErrorModal = ({ title, message, onConfirm }) => {
   return (
     <>
-      <div className={styles.backdrop} onClick={onConfirm} />
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={onConfirm}>Okay</Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <ModalOverlay title={title} message={message} onConfirm={onConfirm} />,
+        document.getElementById('overlay-root'),
+      )}
+      {ReactDOM.createPortal(
+        <BackDrop onConfirm={onConfirm} />,
+        document.getElementById('backdrop-root'),
+      )}
     </>
   );
 };
