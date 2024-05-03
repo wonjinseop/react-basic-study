86c22ff9a7304081986b74e6060ac3675916c2e4
wonjinseop <wonjinseop7@naver.com>
2024-05-03 14:37:00 +0900

    목표설정 App -기본 기능 (목록, 추가, 삭제)

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../UI/Button/Button';
 import './CourseInput.css';
 
-const CourseInput = () => {
+const CourseInput = ({ onAdd }) => {
+  const [enteredText, setEnteredText] = useState('');
+
+  const textChangeHandler = (e) => {
+    setEnteredText(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+
+    onAdd(enteredText);
+
+    setEnteredText('');
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label>나의 목표</label>
-        <input type="text" />
+        <input type="text" onChange={textChangeHandler} value={enteredText} />
       </div>
       <Button type="submit">목표 추가하기</Button>
     </form>
--- src/components/CourseGoals/CourseList.js
+++ src/components/CourseGoals/CourseList.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import CourseItem from './CourseItem';
 import './CourseList.css';
 
-const CourseList = () => {
+const CourseList = ({ items, onDelete }) => {
   return (
     <ul className="goal-list">
-      <li></li>
+      {items.map((item) => {
+        return <CourseItem key={item.id} item={item} onDelete={onDelete} />;
+      })}
     </ul>
   );
 };
