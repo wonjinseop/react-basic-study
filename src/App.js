db415c61c7e9e7af85693fe6caef81e832cd256d
wonjinseop <wonjinseop7@naver.com>
2024-05-07 12:26:54 +0900

    useEffect 기초

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import MainHeader from './components/SideEffect/MainHeader/MainHeader';
 import Login from './components/SideEffect/Login/Login';
 import Home from './components/SideEffect/Home/Home';
 
 const App = () => {
+  console.log('App 컴포넌트 실행!');
   // 로그인 상태를 관리하는 변수
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   // 화면이 리렌더링 될 때 localStorage를 확인해서
   // 현재 login-flag가 존재하는지 검사
   console.log('로그인 검사 수행!');
-  const storedLoginFlag = localStorage.getItem('login-flag');
-  if (storedLoginFlag === '1') {
-    setIsLoggedIn(true);
-  }
+
+  // 기존에 로그인 한 사람인지 확인하는 코드는
+  // 리렌더링 될 때마다 실행하면 안됨! (한 번만 확인하면 됨.)
+  useEffect(() => {
+    console.log('useEffect 실행! - 최초 단 한번만 실행됨!');
+    const storedLoginFlag = localStorage.getItem('login-flag');
+
+    if (storedLoginFlag === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  // 의존성 배열: useEffect가 실행되어야 하는 트리거 변수.
+  // 배열 안에 변수를 지정하면, 해당 변수의 값이 변할 때 마다 useEffect가 실행됨.
+  // 만약 배열을 비워놓으면, 렌더링 과정에서 단 한번만 실행됨.
 
   // 서버로 로그인을 요청하는 함수. (나중에는 fetch를 통한 백엔드와의 연계가 필요.)
   const loginHandler = (email, password) => {
@@ -24,9 +35,15 @@ const App = () => {
     setIsLoggedIn(true);
   };
 
+  const logOutHandler = () => {
+    localStorage.removeItem('login-flag');
+    setIsLoggedIn(false);
+  };
+
+  console.log('App 컴포넌트의 끝!');
   return (
     <>
-      <MainHeader />
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logOutHandler} />
       <main>
         {isLoggedIn && <Home />}
         {!isLoggedIn && <Login onLogin={loginHandler} />}
--- src/components/SideEffect/MainHeader/MainHeader.js
+++ src/components/SideEffect/MainHeader/MainHeader.js
@@ -2,14 +2,11 @@ import React from 'react';
 import classes from './MainHeader.module.css';
 import Navigation from '../Navigation/Navigation';
 
-const MainHeader = (props) => {
+const MainHeader = ({ isAuthenticated, onLogout }) => {
   return (
     <header className={classes['main-header']}>
       <h1>A Typical Page</h1>
-      <Navigation
-        isLoggedIn={props.isAuthenticated}
-        onLogout={props.onLogout}
-      />
+      <Navigation isLoggedIn={isAuthenticated} onLogout={onLogout} />
     </header>
   );
 };
